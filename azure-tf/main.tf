148ea71a2f6427daadb9f59c8c112861fcae692e
Guilherme Chavenco <g.martins882@gmail.com>
2025-02-03 14:14:55 -0300

    Atualização de variaveis tf

--- azure-tf/main.tf
+++ azure-tf/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
   
-  backend "azurerm" {
-    resource_group_name  = "estudacom"
-    storage_account_name = "storageestudacom"
-    container_name      = "estudacom-app"
-    key                 = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "estudacom"
+  #   storage_account_name = "storageestudacom"
+  #   container_name      = "estudacom-app"
+  #   key                 = "terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
